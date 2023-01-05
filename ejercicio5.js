e5451f5fdd12e589cd70302f7b7444fada55125d
davidyanez27 <112655567+davidyanez27@users.noreply.github.com>
2023-01-05 06:31:56 -1100

    Update ejercicio5.js

--- ejercicio5.js
+++ ejercicio5.js
@@ -8,18 +8,34 @@ function llenarArreglo() {
 }
 
 function mostrarArreglo() {
-    array = array.sort()
-    espera= array[0];
-    for (let i = 1; i < array.length; i++) {
-        espera += espera +array[i]+2
+    array = array.sort(function(a, b){return a - b})
+    espera = 0
+    total = 0
 
+    for (i = 0; i < array.length; i++) {
+    
+        i ==0? espera = array[0]: espera =  espera +array[i]+2
+        total +=espera
         
     }
 
-    console.log(array)
-    console.log(espera)
+    console.log(total)
 
     
 }
+
+
+// [4,3,2] 
+// 2=2
+// 2+3+2=7
+// 2+3+2+4=13
+
+// [3,2,5,10,9] 
+
+// 2=2
+// 2+2+3=7
+// 2+2+3+2+5=14
+// 2+2+3+2+5+2+9=25
+// 2+2+3+2+5+2+9+2+10=37
 
 
